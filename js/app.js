2176653f77a0fc645f7ad1a4f3b470bf28d730d6
rhondarobot <rrachellej@gmail.com>
2016-08-05 10:03:41 -0700

    troubleshooting missing js folder

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var quiz = [{
-		question: "When was New York founded"
+		question: "When was New York founded",
 		answers: [1776, 1864, 1624, 1704, 1927],
 		correctAnswer: 2
 }, {
-		question: "What is the capital of New York?"
+		question: "What is the capital of New York?",
 		answers: ["Albany, New York City, Manhattan, Buffalo, Brooklyn"],
 		correctAnswer: 1
 }, {
-		question: "What are the 5 boroughs of New York?"
+		question: "What are the 5 boroughs of New York?",
 		answers: [{"Manhattan, the Bronx, Brooklyn, Albany, Staten Island"},
 			  {"Manhattan, Ellis Island, Brooklyn, Queens, Staten Island"},
 			  {"Albany, the Bronx, Brooklyn, Queens, Staten Island"},
@@ -15,15 +15,15 @@ var quiz = [{
 			  {"Manhattan, the Bronx, Brooklyn, Queens, Staten Island"}],
 		correctAnswer: 4
 	}, {
-		question: "What is New York frequently known as?"
+		question: "What is New York frequently known as?",
 		answers: ["The Subway State, The Windy City, Land of Enchantment, Melting Pot, Sunshine State"],
 		correctAnswer: 3
 	}, {
-		question: "What was New York City originally called?"
+		question: "What was New York City originally called?",
 		answers: ["New England, New Amsterdam, The Big Apple, New America, Land of Liberty"],
 		correctAnswer: 1
 	}, {
-		question: "New York City was the nation's capital from 1789-1790."
+		question: "New York City was the nation's capital from 1789-1790.",
 		answers: ["True, False"],
 		correctAnswer: 0
 }]
@@ -41,10 +41,10 @@ $(document).ready(function() {
  	$('#four').text(quiz[i].answers[4]);
  	$('#five').text(quiz[i].answers[5]);
  	$('#start').remove();
- 	$('#.choices').show('slow');
+ 	$('.choices').show('slow');
  	$('#next').show('slow');
    });
-});
+
 
 
 
